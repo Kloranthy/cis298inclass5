7dba637f63d1e294821a25a9866e400c6ceb1aae
David Barnes <barnesdavidj@gmail.com>
2016-12-07 15:07:57 -0500

    Add part of the JSON crime parser.

--- app/src/main/java/edu/kvcc/cis298/criminalintent/CrimeFetcher.java
+++ app/src/main/java/edu/kvcc/cis298/criminalintent/CrimeFetcher.java
@@ -3,11 +3,21 @@ package edu.kvcc.cis298.criminalintent;
 import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
 
 /**
  * Created by dbarnes on 12/5/2016.
@@ -87,10 +97,59 @@ public class CrimeFetcher {
                     //.appendQueryParameter("param", "Value")
                     .build().toString();
 
+            //This calls the above methods to use the URL to get the JSON from
+            //the web service. After this call we will actually have the JSON
+            //that we need to parse
             String jsonString = getUrlString(url);
+
+            //This will take the jsonString that we got back and put it into
+            //a jsonArray object. We have to use a jsonArray because our
+            //jsonString starts out with an Array. If it started with an Object
+            //"{}" we would need to use JSONObject instead of JSONArray. The
+            //book uses JSONObject for their parse when they use Flickr.
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            //Parse the crimes out from the object.
+
             Log.i(TAG, "Fetched contents of URL: " + jsonString);
+        } catch (JSONException jse) {
+            Log.e(TAG, "Failed to parse JSON", jse);
         } catch (IOException ioe) {
             Log.e(TAG, "Failed to load", ioe);
+        }
+    }
+
+    private void parseCrimes(List<Crime> crimes, JSONArray jsonArray)
+        throws IOException, JSONException {
+
+        //Loop through all of the elements in the JSONArray that were sent
+        //into this method
+        for (int i=0; i<jsonArray.length(); i++) {
+
+            //Fetch a single JSONObject out from the JSONArray based on
+            //the current index that we are in
+            JSONObject crimeJsonObject = jsonArray.getJSONObject(i);
+
+            //Pull the Value from the JSONObject for the Key of "uuid"
+            String uuidString = crimeJsonObject.getString("uuid");
+            //Use the Value to create a new UUID from that string
+            UUID uuidForNewCrime = UUID.fromString(uuidString);
+
+            //Get out the title from the JSONObject
+            String title = crimeJsonObject.getString("title");
+
+            //Get out the date and try to parse it.
+            try {
+                //Declare a date formatter that can be used to parse the date from
+                //a string into an actual date object.
+                DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+                //Use the format to parse the string that we get from the JSONObject
+                Date date = format.parse(crimeJsonObject.getString("incident_date"));
+
+            } catch (Exception e) {
+                
+            }
+
         }
     }
 
